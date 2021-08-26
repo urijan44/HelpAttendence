565fe578efca200076557aaf57cc2046f4236017
이호승 <urijan44@daum.net>
2021-08-26 16:22:21 +0900

    출근을 부탁해 프로젝트 생성 버스 리스트 뷰 배치

---
+++ HelpAttendence/Models/Bus.swift
@@ -0,0 +1,13 @@
+//
+//  Bus.swift
+//  HelpAttendence
+//
+//  Created by hoseung Lee on 2021/08/26.
+//
+
+import Foundation
+
+struct BusModel: Codable {
+  var local: String
+  var number: String
+}
---
+++ HelpAttendence/ViewControllers/BusListView.swift
@@ -0,0 +1,20 @@
+//
+//  BusListView.swift
+//  HelpAttendence
+//
+//  Created by hoseung Lee on 2021/08/26.
+//
+
+import UIKit
+
+class BusListView: UITableViewController {
+  private struct Constants {
+    
+  }
+  
+//  var busImage: UIImage = Constants.defaultBusImage
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
